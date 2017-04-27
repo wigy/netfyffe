13f9a4ba8d3d0b6bf5908265bee7549a5a30e48e
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2017-04-27 20:51:16 +0300

    Split css and template to separate files.

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>netFyffe</h1><router-outlet></router-outlet>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent  {
 }
