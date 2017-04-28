536a6b35ab59e0629931d6c9089680ff54d995c9
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2017-04-28 16:17:13 +0300

    Display some account data.

--- ui/src/app/pages/accounts.ts
+++ ui/src/app/pages/accounts.ts
@@ -3,15 +3,17 @@ import { PortfolioService } from '../services/portfolio.service';
 import { AccountGroup } from '../models/account_group';
 
 @Component({
-  template: `<h2>Accounts</h2>`,
+  templateUrl: './accounts.html',
 })
 export class AccountsComponent implements OnInit  {
+
+  accountGroups: AccountGroup[];
 
   constructor(private portfolio: PortfolioService) {
   }
 
   ngOnInit(): void {
     this.portfolio.getAccounts()
-      .then((data: AccountGroup[]) => console.log(data));
+      .then((data: AccountGroup[]) => this.accountGroups = data);
   }
 }
