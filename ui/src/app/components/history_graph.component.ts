81910806268f7f0e1c29043cba2b2e89977c0699
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2017-05-01 14:20:21 +0300

    Pass accounts as a parameter to graph.

--- ui/src/app/components/history_graph.component.ts
+++ ui/src/app/components/history_graph.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AccountGroup } from '../models/account_group';
 
 @Component({
   selector: 'history-graph',
-  template: 'GRAPH HERE',
+  template: 'GRAPH HERE ({{accounts[0].name}})',
 })
 export class HistoryGraphComponent  {
+
+  @Input() accounts: AccountGroup[];
 }
