9949e9fb302cbf52d91607449496b4f692a5e560
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2017-05-25 17:02:48 +0300

    Initialize fixed dates at the same time.

--- ui/src/app/pages/history.ts
+++ ui/src/app/pages/history.ts
@@ -18,7 +18,7 @@ export class HistoryComponent implements OnInit  {
   constructor(private portfolioService: PortfolioService, private route: ActivatedRoute) {
     this.portfolio = new Portfolio();
     this.data = [];
-    this.recentDates = Dates.make(['1D', '1W', '1M', '3M', '6M', 'YTD', '1Y', '3Y', '5Y']);
+    this.recentDates = [];
     this.quarterDates = [];
   }
 
@@ -29,6 +29,7 @@ export class HistoryComponent implements OnInit  {
           this.portfolio = portfolio;
           // TODO: Calculate quarter dates applicapable to the portfolio.
           this.quarterDates = Dates.make(['2016Q1', '2016Q2', '2016Q3', '2016Q4', '2017Q1', '2017Q2']);
+          this.recentDates = Dates.make(['1D', '1W', '1M', '3M', '6M', 'YTD', '1Y', '3Y', '5Y']);
         });
     });
   }
