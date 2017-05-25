a6b65d36ec69c15669b7a65d5dd6e195dd192a75
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2017-05-25 16:51:55 +0300

    Drop debug.

--- ui/src/app/pages/history.ts
+++ ui/src/app/pages/history.ts
@@ -28,15 +28,7 @@ export class HistoryComponent implements OnInit  {
         .then(portfolio => {
           this.portfolio = portfolio;
           // TODO: Calculate quarter dates applicapable to the portfolio.
-          this.quarterDates = Dates.make(['2016Q1', '2016Q2', '2016Q3', '2016Q4', '2017Q1', '2016Q2']);
-
-          // TODO: Just testing here.
-          let q1 = Query.build('2016-06-01');
-          let q2 = Query.build('2016-08-04');
-          let q3 = Query.build('2016-08-03');
-          let q4 = Query.build('2017-01-01', '2017-04-30');
-          d(portfolio.query(q4));
-
+          this.quarterDates = Dates.make(['2016Q1', '2016Q2', '2016Q3', '2016Q4', '2017Q1', '2017Q2']);
         });
     });
   }
