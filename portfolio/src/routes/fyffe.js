3fac26e3f53ff981e13c2ff33a66ede2798b6dd1
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2017-08-08 19:59:06 +0300

    More docs.

--- portfolio/src/routes/fyffe.js
+++ portfolio/src/routes/fyffe.js
@@ -12,48 +12,49 @@ const query = require('../lib/db/query');
  * Collect all information of the portfolio.
  *
  * @apiSuccessExample Success-Response:
- * {
- *     "accounts": [
- *         {
- *             "id": 1,
- *             "account_group_id": 1,
- *             "currency": "EUR"
- *         }
- *     ],
- *     "account_groups": [
- *         {
- *             "id": 1,
- *             "bank_id": 1,
- *             "name": "Euro Account",
- *             "code": "12345"
- *         }
- *     ],
- *     "balances": {
- *         "1": {
- *             "2016-08-01": 200000,
- *             "2016-08-03": 149800,
- *             "2016-09-14": 48836,
- *             "2017-03-24": 126781
- *         }
- *     },
- *     "instruments": [
- *         {
- *             "id": 1,
- *             "account_id": 1,
- *             "ticker": "HEL:NOKIA",
- *             "count": 100,
- *             "buy_price": 50200,
- *             "sell_price": null,
- *             "bought": "2016-08-03",
- *             "sold": null
- *         }
- *     ],
- *     "capital": {
- *         "1": {
- *             "2016-08-01": 200000
+ *     HTTP/1.1 200 OK
+ *     {
+ *         "accounts": [
+ *             {
+ *                 "id": 1,
+ *                 "account_group_id": 1,
+ *                 "currency": "EUR"
+ *             }
+ *         ],
+ *         "account_groups": [
+ *             {
+ *                 "id": 1,
+ *                 "bank_id": 1,
+ *                 "name": "Euro Account",
+ *                 "code": "12345"
+ *             }
+ *         ],
+ *         "balances": {
+ *             "1": {
+ *                 "2016-08-01": 200000,
+ *                 "2016-08-03": 149800,
+ *                 "2016-09-14": 48836,
+ *                 "2017-03-24": 126781
+ *             }
+ *         },
+ *         "instruments": [
+ *             {
+ *                 "id": 1,
+ *                 "account_id": 1,
+ *                 "ticker": "HEL:NOKIA",
+ *                 "count": 100,
+ *                 "buy_price": 50200,
+ *                 "sell_price": null,
+ *                 "bought": "2016-08-03",
+ *                 "sold": null
+ *             }
+ *         ],
+ *         "capital": {
+ *             "1": {
+ *                 "2016-08-01": 200000
+ *             }
  *         }
  *     }
- * }
  */
 router.get('/', (req, res) => {
 
