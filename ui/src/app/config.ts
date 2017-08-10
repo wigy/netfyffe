c7e30e0afbb712ffeede771937f1041cd9f001af
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2017-08-10 21:39:54 +0300

    Disable tslint from generated file.

--- ui/src/app/config.ts
+++ ui/src/app/config.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 ///////////////////////////////////////////////
 /// AUTOMATICALLY GENERATED - DO NOT CHANGE ///
 ///////////////////////////////////////////////
+/* tslint:disable */
 @Injectable()
 export class Config {
+
     public PORTFOLIO_URL: string;
 
     constructor() {
         this.PORTFOLIO_URL = "http://localhost:9002";
     }
 }
+/* tslint:enable */
