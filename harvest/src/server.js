6dc515a9755c3e93fd96d5eb3b590edcc5c0afe2
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2017-08-15 19:56:21 +0300

    Docker definitions (partial) for harvest.

--- harvest/src/server.js
+++ harvest/src/server.js
@@ -3,7 +3,7 @@ const config = require('./config');
 const app = express();
 
 global.d = require('neat-dump');
-
+// TODO: NETFYFFE_HARVEST needs to be changed to HARVEST_MODULE
 if (!config.harvester_module) {
     throw new Error("The harvester module is not defined in NETFYFFE_HARVEST environment variable.");
 }
