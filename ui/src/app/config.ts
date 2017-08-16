5383f377c949fd6a4e829d5efc0767dc42ec50e9
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2017-08-16 19:19:05 +0300

    Configurable quores URL.

--- ui/src/app/config.ts
+++ ui/src/app/config.ts
@@ -7,9 +7,11 @@ import { Injectable } from '@angular/core';
 export class Config {
 
     public PORTFOLIO_URL: string;
+    public QUOTES_URL: string;
 
     constructor() {
         this.PORTFOLIO_URL = "http://localhost:9002";
+        this.QUOTES_URL = "http://localhost:9000";
     }
 }
 /* tslint:enable */
