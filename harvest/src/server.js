ce632087d35a26734437fed8b0370ddd7a7438da
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2017-08-16 19:01:11 +0300

    Handle harvest module in docker environment.

--- harvest/src/server.js
+++ harvest/src/server.js
@@ -5,9 +5,9 @@ const app = express();
 global.d = require('neat-dump');
 // TODO: NETFYFFE_HARVEST needs to be changed to HARVEST_MODULE
 if (!config.harvester_module) {
-    throw new Error("The harvester module is not defined in NETFYFFE_HARVEST environment variable.");
+  throw new Error("The harvester module is not defined in NETFYFFE_HARVEST environment variable.");
 }
-
+d.info("Using harvest-module", config.harvester_module);
 app.use(d.middleware());
 app.use('/doc', express.static('./doc'));
 app.use('/ticker', require('./routes/ticker'));
