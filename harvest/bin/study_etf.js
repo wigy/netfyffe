8cfdd2548d38ff644b3b84a9e581f3b6048e2063
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2017-10-24 20:26:00 +0300

    Start work on etf explorer.

--- harvest/bin/study_etf.js
+++ harvest/bin/study_etf.js
@@ -5,5 +5,46 @@
 
 global.d = require('neat-dump');
 const engine = require('../src/engine');
+const readline = require('readline');
 
-engine.init();
+async function select(data, field, msg = 'Select one: ') {
+    if (data.length < 1) {
+        d.error('No data found.');
+        process.exit();
+    }
+    if (data.length > 1) {
+        let n = 1;
+        data.forEach(line => {
+            console.log(n + '.', line[field]);
+            console.log('  ', JSON.stringify(line));
+            n++;
+        });
+    }
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    return new Promise((resolve, reject) => {
+        rl.question(msg, (answer) => {
+            resolve(data[parseInt(answer) - 1]);
+            rl.close();
+        });
+    });
+}
+
+if (process.argv.length < 3) {
+    console.log('');
+    console.log('Usage: study_etf.js <ETF name>');
+    console.log('');
+} else {
+    engine.init();
+    const lookup = process.argv.slice(2).join(' ');
+    d('Looking for', lookup);
+    engine.getTickerSearch(lookup)
+    .then((data) => {
+            return select(data, 'name');
+        })
+        .then((data) => {
+            console.log(JSON.stringify(data, null, 2));
+        });
+}
