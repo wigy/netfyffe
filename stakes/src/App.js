9f72510ac245ff112af242ca777f2b2e653c3995
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2018-05-06 15:10:21 +0300

    Minimal project setup.

---
+++ stakes/src/App.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var app = require('express')();
+exports["default"] = app;
---
+++ stakes/src/App.ts
@@ -0,0 +1,4 @@
+import { Request, Response, Application, Router } from 'express';
+const app: Application = require('express')();
+
+export default app;
