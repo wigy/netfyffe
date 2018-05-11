12b561dc8a38840e22e8acb7123bc0771a65ac33
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2018-05-11 14:03:39 +0300

    Use namespace instead of static class member.

--- stakes/src/API/ClassRouter.ts
+++ stakes/src/API/ClassRouter.ts
@@ -3,9 +3,9 @@ import { Request, Response, Router, RequestHandler } from 'express';
 import { DataObject } from '../Common/DataObject';
 import { Inherits } from '../Common/Types/Inherits';
 
-export class ClassRouter {
+export namespace ClassRouter {
 
-  static forClass(tableName: string, targetClass: Inherits<DataObject>) : Router {
+  export function forClass(tableName: string, targetClass: Inherits<DataObject>) : Router {
 
     const config = require('../../knexfile.js');
     const router: Router = Router();
