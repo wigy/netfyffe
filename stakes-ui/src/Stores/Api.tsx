783b021202f02cfb516493cebb6d08a85a950797
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2018-05-11 16:16:04 +0300

    Sample API.

--- stakes-ui/src/Stores/Api.tsx
+++ stakes-ui/src/Stores/Api.tsx
@@ -1,15 +1,15 @@
-// import { Investor } from '../Common/Investor';
+import { Investor } from '../Common/Investor';
 
 class Api  {
 
   public static getAll() : Promise<any> {
-
+    // TODO: Make configurable.
     return fetch('http://localhost:9003/investors')
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        console.log(data);
+        console.log(data.map((d: any) => new Investor(d)));
       })
       .catch((err) => {
         console.error('API ERROR:', err);
