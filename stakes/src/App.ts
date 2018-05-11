fff334bd29d7ded3fae43d2cc55d5b7103793321
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2018-05-11 15:38:03 +0300

    Use cors.

--- stakes/src/App.ts
+++ stakes/src/App.ts
@@ -5,6 +5,13 @@ import { Account } from './Common/Account';
 
 const app: Application = require('express')();
 
+app.use(require('cors')());
+
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+});
+
 app.use('/investors', ClassRouter.forClass('investors', Investor));
 app.use('/accounts', ClassRouter.forClass('accounts', Account));
 
