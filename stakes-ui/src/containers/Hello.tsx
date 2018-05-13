188c0c0a410457d917f3e14c8f8b8dc0e9e2f148
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2018-05-13 18:46:55 +0300

    Example copied from internet.

---
+++ stakes-ui/src/containers/Hello.tsx
@@ -0,0 +1,19 @@
+import Hello from '../components/Hello';
+import * as actions from '../actions/';
+import { StoreState } from '../types/index';
+import { connect, Dispatch } from 'react-redux';
+
+export function mapStateToProps({ enthusiasmLevel, languageName }: StoreState) {
+  return {
+    enthusiasmLevel,
+    name: languageName,
+  }
+}
+export function mapDispatchToProps(dispatch: Dispatch<actions.EnthusiasmAction>) {
+  return {
+    onDecrement: () => dispatch(actions.decrementEnthusiasm()),
+    onIncrement: () => dispatch(actions.incrementEnthusiasm()),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Hello);
--- stakes-ui/src/index.tsx
+++ stakes-ui/src/index.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+// import App from './App';
+// import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+
+import Hello from './containers/Hello';
+
+import { createStore } from 'redux';
+import { enthusiasm } from './reducers/index';
+// import { StoreState } from './types/index';
+
+const store = createStore(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
