1ffcf46b9c25ad4bfe7be0a5f0ee887e9400be0a
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2018-05-29 20:06:28 +0300

    Handler to find selected investor to view.

--- stakes-ui/src/components/InvestorView.tsx
+++ stakes-ui/src/components/InvestorView.tsx
@@ -13,14 +13,17 @@ export interface Props {
 function InvestorView({ investor }: Props) {
   return (
     <div className="InvestorView">
-      Investor
+      <h1>{investor ? investor.fullName : ''}</h1>
     </div>
   );
 }
 
-function mapStateToProps({ investor }: StoreState) {
+// TODO: Type of ownProps?
+function mapStateToProps({ investors }: StoreState, ownProps: any) {
+  const id = parseInt(ownProps.match.params.id, 10);
+  const match = investors.filter(investor => investor.id === id);
   return {
-    investor
+    investor: match.length ? match[0] : new Investor({id: 0})
   }
 }
 
