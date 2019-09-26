f0f160ddc5e827f438993af102b153ba49b3e3b7
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2019-09-26 13:29:36 +0300

    Change the way to configure.

--- harvest/src/config.js
+++ harvest/src/config.js
@@ -1,29 +1,10 @@
-require('dotenv').config();
-// TODO: This is not good way of configuring.
-const config = {
-    develop: {
-        // Server port
-        port: process.env.PORT || 9001,
-        // Colon separated paths to the harverster modules.
-        harvestModules: process.env.HARVEST_MODULES,
-        // Kraken API key.
-        krakenApiKey: process.env.KRAKEN_API_KEY,
-        // Kraken API private key.
-        krakenApiPrivateKey: process.env.KRAKEN_API_PRIVATE_KEY,
-    },
-
-    production: {
-        // Server port
-        port: process.env.PORT || 9001,
-        // Colon separated paths to the harverster modules.
-        harvestModules: process.env.HARVEST_MODULES,
-        // Kraken API key.
-        krakenApiKey: process.env.KRAKEN_API_KEY,
-        // Kraken API private key.
-        krakenApiPrivateKey: process.env.KRAKEN_API_PRIVATE_KEY,
-    },
+module.exports = {
+    // Server port
+    port: process.env.PORT || 9001,
+    // Colon separated paths to the harvester modules.
+    harvestModules: process.env.HARVEST_MODULES,
+    // Kraken API key.
+    krakenApiKey: process.env.KRAKEN_API_KEY,
+    // Kraken API private key.
+    krakenApiPrivateKey: process.env.KRAKEN_API_PRIVATE_KEY,
 };
-
-const env = process.env.NODE_ENV || 'develop';
-
-module.exports = config[env];
